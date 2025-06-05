f7e60350cdb8093bd83cd9e497525935562d2c5a
Andres036036 <didjudke@gmail.com>
2025-06-05 06:02:57 -0500

    Add files via upload

--- consultas.sql
+++ consultas.sql
@@ -1,14 +1,16 @@
-DROP DATABASE IF EXISTS consultuas;
+DROP DATABASE IF EXISTS consultas;
 CREATE DATABASE consultas;
 USE consultas;
+
 CREATE TABLE fabricante (
-codigo INT PRIMARY KEY AUTO_INCREMENT,
-nombre VARCHAR (100)
+  codigo INT PRIMARY KEY AUTO_INCREMENT,
+  nombre VARCHAR(100)
 );
+
 CREATE TABLE producto (
-codigo INT PRIMARY KEY AUTO_INCREMENT,
-nombre VARCHAR (100),
-precio DOUBLE,
-fabricante INT,
-FOREIGN KEY (fabricante) REFERENCES fabricante (codigo)
+  codigo INT PRIMARY KEY AUTO_INCREMENT,
+  nombre VARCHAR(100),
+  precio DOUBLE,
+  codigo_fabricante INT,
+  CONSTRAINT fk_codigo_fabricante FOREIGN KEY (codigo_fabricante) REFERENCES fabricante(codigo)
 );
--- dml.sql
+++ dml.sql
@@ -1,36 +1,20 @@
-USE `consultas`;
-DROP procedure IF EXISTS `fabricante`;
-
 DELIMITER $$
-USE `consultas`$$
-CREATE PROCEDURE `fabricante` (
- IN nombre VARCHAR (100)
+CREATE PROCEDURE insertar_fabricante (
+  IN nombre_fab VARCHAR(100)
 )
 BEGIN
-  INSERT INTO fabricante (nombre)
-  VALUES (nombre);
+  INSERT INTO fabricante(nombre) VALUES (nombre_fab);
 END$$
-
 DELIMITER ;
 
-
-USE `consultas`;
-DROP procedure IF EXISTS `producto`;
-
-USE `consultas`;
-DROP procedure IF EXISTS `consultas`.`producto`;
-;
-
 DELIMITER $$
-USE `consultas`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `producto`(
- IN nombre VARCHAR (100),
- IN precio DOUBLE,
- IN fabricante int
+CREATE PROCEDURE insertar_producto (
+  IN nombre_prod VARCHAR(100),
+  IN precio_prod DOUBLE,
+  IN cod_fab INT
 )
 BEGIN
- INSERT INTO producto (nombre,precio,fabricante) 
- VALUES (nombre,precio,fabricante);
+  INSERT INTO producto(nombre, precio, codigo_fabricante) 
+  VALUES (nombre_prod, precio_prod, cod_fab);
 END$$
-
 DELIMITER ;
